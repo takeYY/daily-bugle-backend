d26606a299e4f1059c8546eee6c19920b34a8e84
takeYY <t0k6yist@gmail.com>
2022-05-13 16:08:54 +0900

    ⚡️ (app, todos): firestoreの初期化をappに集約

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+export const firestore = admin.firestore();
 
 @Injectable()
 export class AppService {
--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import * as admin from 'firebase-admin';
 
-admin.initializeApp();
-const firestore = admin.firestore();
+import { firestore } from '../app.service';
+
 const collectionRef = firestore.collection('todos');
 
 @Injectable()
