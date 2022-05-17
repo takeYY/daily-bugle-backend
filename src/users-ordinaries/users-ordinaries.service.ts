1d3201469231cdd297451b3fa591868363918756
takeYY <t0k6yist@gmail.com>
2022-05-17 11:09:14 +0900

    ✨ (users-ordinaries): create API作成

--- src/users-ordinaries/users-ordinaries.service.ts
+++ src/users-ordinaries/users-ordinaries.service.ts
@@ -2,10 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { CreateUsersOrdinaryDto } from './dto/create-users-ordinary.dto';
 import { UpdateUsersOrdinaryDto } from './dto/update-users-ordinary.dto';
 
+import { firestore } from '../app.service';
+
+const collectionRef = firestore.collection('users-ordinaries');
+
 @Injectable()
 export class UsersOrdinariesService {
-  create(createUsersOrdinaryDto: CreateUsersOrdinaryDto) {
-    return 'This action adds a new usersOrdinary';
+  async create(createUsersOrdinaryDto: CreateUsersOrdinaryDto) {
+    const docRef = await collectionRef.add({
+      userId: createUsersOrdinaryDto.userId,
+      ordinaryId: createUsersOrdinaryDto.ordinaryId,
+      weekdayId: createUsersOrdinaryDto.weekdayId,
+      startedOn: createUsersOrdinaryDto.startedOn,
+      createdAt: createUsersOrdinaryDto.createdAt,
+      updatedAt: createUsersOrdinaryDto.updatedAt,
+    });
+    const snapshot = await docRef.get();
+    const data = snapshot.data();
+    const newUsersOrdinaryData = {
+      id: docRef.id,
+      ...data,
+    };
+
+    return newUsersOrdinaryData;
   }
 
   findAll() {
