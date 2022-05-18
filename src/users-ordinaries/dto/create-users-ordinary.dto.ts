abd77d0461002481f6f90f9e1a04300ba18fae16
takeYY <t0k6yist@gmail.com>
2022-05-19 00:37:01 +0900

    🏷️ (usersOrdinary): ordinaryとweekdaysを小要素に追加

--- src/users-ordinaries/dto/create-users-ordinary.dto.ts
+++ src/users-ordinaries/dto/create-users-ordinary.dto.ts
@@ -1,8 +1,13 @@
+import { CreateOrdinaryDto } from 'src/ordinaries/dto/create-ordinary.dto';
+import { CreateWeekdayDto } from 'src/weekdays/dto/create-weekday.dto';
+
 export class CreateUsersOrdinaryDto {
+  id?: string;
   userId: string;
-  ordinaryId: string;
-  weekdayId: string;
+  ordinary: CreateOrdinaryDto;
+  weekdays: CreateWeekdayDto[];
   startedOn: Date;
   createdAt: Date;
   updatedAt: Date;
+  isClosed: boolean;
 }
