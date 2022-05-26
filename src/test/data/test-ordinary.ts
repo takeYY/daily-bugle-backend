04d65c8d3bcfdaddc5dd876633e8c84f4c71c4c6
takeYY <t0k6yist@gmail.com>
2022-05-26 09:08:19 +0900

    ✅ (ordinary): テスト用のデータ作成

---
+++ src/test/data/test-ordinary.ts
@@ -0,0 +1,9 @@
+import { CreateOrdinaryDto } from 'src/ordinaries/dto/create-ordinary.dto';
+
+export const testOrdinaries: CreateOrdinaryDto[] = [
+  { name: '朝食を摂る' },
+  { name: '歯を磨く' },
+  { name: '掃除をする' },
+  { name: '洗濯をする' },
+  { name: '買い物に出かける' },
+];
