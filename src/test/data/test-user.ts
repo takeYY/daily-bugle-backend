fabee53f580f4349c7eacc54fe4e043a35bd4a1a
takeYY <t0k6yist@gmail.com>
2022-05-26 09:07:57 +0900

    ✅ (user): テスト用のデータ作成

---
+++ src/test/data/test-user.ts
@@ -0,0 +1,9 @@
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+export const testUsers: CreateUserDto[] = [
+  { displayName: 'user-1', photoDataUrl: '' },
+  { displayName: 'user-2', photoDataUrl: '' },
+  { displayName: 'user-3', photoDataUrl: '' },
+  { displayName: 'user-4', photoDataUrl: '' },
+  { displayName: 'user-5', photoDataUrl: '' },
+];
